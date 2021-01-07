a307e6f22f475829d87d50760f1212f2b94d4556
Chungjun Lee <chunglee3224@gmail.com>
2021-01-07 16:56:30 +0900

    split model and DNN_classifier

--- ad_model.py
+++ ad_model.py
@@ -4,16 +4,27 @@ import numpy as np
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-class AD_SUP2_MODEL1(nn.Module):
-    def __init__(self, reduce):
-        super(AD_SUP2_MODEL1, self).__init__()
-
+class DNN_classifier(nn.Module):
+    def __init__(self):
+        super(DNN_classifier, self).__init__()
         self.fc1 = nn.Linear(22, 600)
         self.fc2 = nn.Linear(600, 600)
         self.fc3 = nn.Linear(600, 600)
         self.fc4 = nn.Linear(600, 2)
         self.relu = nn.ReLU()
 
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.relu(self.fc3(x))
+        x = self.fc4(x)
+        return F.log_softmax(x, dim=1)
+
+class AD_SUP2_MODEL1(nn.Module):
+    def __init__(self, reduce):
+        super(AD_SUP2_MODEL1, self).__init__()
+
+        self.classifier = DNN_classifier()
         self.reduce = reduce # either 'mean' or 'max'
 
     def encoder(self, annotation):
@@ -26,20 +37,6 @@ class AD_SUP2_MODEL1(nn.Module):
             import sys; sys.exit(-1)
 
         return enc_out
-
-    def classifier(self, enc_out):
-        a1 = self.fc1(enc_out)
-        h1 = self.relu(a1)
-
-        a2 = self.fc2(h1)
-        h2 = self.relu(a2)
-
-        a3 = self.fc3(h2)
-        h3 = self.relu(a3)
-
-        out = self.fc4(h3)
-
-        return F.log_softmax(out, dim=1)
 
     def forward(self, annotation):
 
